b6cfa1573e81d1f3d559239df04c01ee8c5ab378
puechua <puechua@gmail.com>
2023-10-10 16:58:43 +0200

    Dodaj pliki projektów.

---
+++ Conversion/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float myFloatValue = 10;
+            int myIntValue = (int) myFloatValue;
+            Console.WriteLine("myIntValue rowna sie " + myIntValue);
+        }
+    }
+}
